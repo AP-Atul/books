ad9d97d46c3c945bd9f5d78875ffcbb8c3bbf2ae
atul <atulpatare99@gmail.com>
2024-04-27 00:02:35 +0530

    refactor(books): add labels to schema

--- src/books/schema.ts
+++ src/books/schema.ts
@@ -1,27 +1,37 @@
 import joi from 'joi'
 
-export const create = joi.object({
-  title: joi.string().required(),
-  author: joi.string().required(),
-  isbn: joi.string().required(),
-  publication_date: joi.date().iso().required()
-})
+export const create = joi
+  .object({
+    title: joi.string().required(),
+    author: joi.string().required(),
+    isbn: joi.string().required(),
+    publication_date: joi.date().iso().required()
+  })
+  .label('Create book')
 
-export const list = joi.object({
-  page: joi.number().default(1)
-})
+export const list = joi
+  .object({
+    page: joi.number().default(1)
+  })
+  .label('List books')
 
-export const delete_ = joi.object({
-  id: joi.string().uuid().required()
-})
+export const delete_ = joi
+  .object({
+    id: joi.string().uuid().required()
+  })
+  .label('Delete book')
 
-export const id = joi.object({
-  id: joi.string().uuid().required()
-})
+export const id = joi
+  .object({
+    id: joi.string().uuid().required()
+  })
+  .label('Id')
 
-export const update = joi.object({
-  title: joi.string(),
-  author: joi.string(),
-  isbn: joi.string(),
-  publication_date: joi.date().iso()
-})
+export const update = joi
+  .object({
+    title: joi.string(),
+    author: joi.string(),
+    isbn: joi.string(),
+    publication_date: joi.date().iso()
+  })
+  .label('Update book')
